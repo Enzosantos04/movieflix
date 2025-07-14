ac796cd455ee1a87e71e103624aa88c40fbecbac
enzosdev <enzohfs@gmail.com>
2025-07-14 18:39:01 -0300

    feat: configura o banco de dados (postgreSQL)  da aplicacao com variaveis de ambiente

--- src/main/java/com/movieflix/MovieFlixApplication.java
+++ src/main/java/com/movieflix/MovieFlixApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieFlixApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieFlixApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFlixApplication.class, args);
+    }
 
 }
