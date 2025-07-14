ce818c8b5e1f19d1510effa8c230cd1694eb60fd
enzosdev <enzohfs@gmail.com>
2025-07-14 18:42:00 -0300

    fix: corrige erro de digitacao

--- src/main/java/com/movieflix/MovieFlixApplication.java
+++ src/main/java/com/movieflix/MovieFlixApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieFlixApplicatio {
+public class MovieFlixApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MovieFlixApplication.class, args);
