4c7fa4770b0b7fb63be7bc9c16b421a5f8b946ec
enzosdev <enzohfs@gmail.com>
2025-07-14 19:04:43 -0300

    minor changes

--- src/test/java/com/movieflix/MovieFlixApplicationTests.java
+++ src/test/java/com/movieflix/MovieFlixApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MovieFlixApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
