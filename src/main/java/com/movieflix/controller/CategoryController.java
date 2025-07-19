606542035501d8a4721fd7228fd7656100751c8e
enzosdev <enzohfs@gmail.com>
2025-07-19 20:52:59 -0300

    feat: cria Controller para a entidade category com os endpoints getAllCategories e saveCategory

--- src/main/java/com/movieflix/controller/CategoryController.java
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -1,4 +1,27 @@
 package com.movieflix.controller;
 
+import com.movieflix.entity.Category;
+import com.movieflix.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movieflix/category")
 public class CategoryController {
+   private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping()
+    public List<Category> getAllCategories(){
+       return categoryService.findAll();
+    }
+
+    @PostMapping
+    public Category saveCategory(@RequestBody Category category){
+        return categoryService.saveCategory(category);
+    }
 }
