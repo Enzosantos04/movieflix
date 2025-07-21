a4afc826b2ab21300ab4236b0d8402e28e5ee760
enzosdev <enzohfs@gmail.com>
2025-07-21 13:56:52 -0300

    feat: cria mapper que converte  a entidade streaming em DTO

--- src/main/java/com/movieflix/mapper/StreamingMapper.java
+++ src/main/java/com/movieflix/mapper/StreamingMapper.java
@@ -1,4 +1,26 @@
 package com.movieflix.mapper;
 
-public class StreamingMapper {
+
+
+import com.movieflix.dto.StreamingDTO;
+import com.movieflix.entity.Streaming;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StreamingMapper{
+
+    public Streaming map(StreamingDTO streamingDTO){
+        Streaming streaming =  new Streaming();
+        streaming.setId(streamingDTO.getId());
+        streaming.setName(streamingDTO.getName());
+        return streaming;
+    }
+
+
+    public StreamingDTO map(Streaming streaming){
+        StreamingDTO streamingDTO = new StreamingDTO();
+        streamingDTO.setId(streaming.getId());
+        streamingDTO.setName(streaming.getName());
+        return streamingDTO;
+    }
 }
