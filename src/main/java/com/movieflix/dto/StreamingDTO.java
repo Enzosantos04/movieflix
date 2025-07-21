7a8ac566ca343c8e3d49c95dbf7bc127977e07c4
enzosdev <enzohfs@gmail.com>
2025-07-21 13:53:51 -0300

    feat: Cria DTO para entidade Streaming

--- src/main/java/com/movieflix/dto/StreamingDTO.java
+++ src/main/java/com/movieflix/dto/StreamingDTO.java
@@ -1,4 +1,15 @@
 package com.movieflix.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StreamingDTO {
+    private Long id;
+    private String name;
 }
