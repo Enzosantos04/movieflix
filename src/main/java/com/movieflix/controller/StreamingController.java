8fb431bd3448f1d2d2f1cf44fc7f9570950fd474
enzosdev <enzohfs@gmail.com>
2025-07-21 16:10:29 -0300

    feat: Controller para entidade streaming com os endpoints getStreamingById, getAllStreamings, saveStreaming e deleteStreamingById

--- src/main/java/com/movieflix/controller/StreamingController.java
+++ src/main/java/com/movieflix/controller/StreamingController.java
@@ -1,4 +1,59 @@
 package com.movieflix.controller;
 
+
+import com.movieflix.dto.StreamingDTO;
+import com.movieflix.service.StreamingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movieflix/streaming")
 public class StreamingController {
+
+    private final StreamingService streamingService;
+
+    public StreamingController(StreamingService streamingService) {
+        this.streamingService = streamingService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StreamingDTO>> getAllStreamings(){
+        List<StreamingDTO> streamings = streamingService.findAll();
+        return ResponseEntity.ok(streamings);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> saveStreaming(@RequestBody StreamingDTO streaming){
+        StreamingDTO streamingDTO = streamingService.saveStreaming(streaming);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Streaming Saved with success. " + streamingDTO.getName());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getStreamingById(@PathVariable Long id){
+        if(streamingService.getStreamingById(id) != null){
+           StreamingDTO streamingDTO = streamingService.getStreamingById(id);
+           return ResponseEntity.ok(streamingDTO);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Streaming Not Found");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteStreamingById(@PathVariable Long id){
+        if(streamingService.getStreamingById(id) != null){
+            streamingService.deleteStreamingById(id);
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Streaming Not Found");
+        }
+    }
+
+
+
+
+
 }
