55510ccdae2b8de9304b359bb034c3fecec8ba8a
enzosdev <enzohfs@gmail.com>
2025-07-21 13:55:26 -0300

    feat: cria entidade Streaming

--- src/main/java/com/movieflix/entity/Streaming.java
+++ src/main/java/com/movieflix/entity/Streaming.java
@@ -1,4 +1,19 @@
 package com.movieflix.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="streaming")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Streaming {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String name;
 }
