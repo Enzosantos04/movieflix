8a265dea5f1e2e0a21cd3d6e88434b80439855a7
enzosdev <enzohfs@gmail.com>
2025-07-21 20:02:02 -0300

    feat: cria DTO para entidade Movie

--- src/main/java/com/movieflix/dto/MovieDTO.java
+++ src/main/java/com/movieflix/dto/MovieDTO.java
@@ -1,4 +1,26 @@
 package com.movieflix.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MovieDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate releaseDate;
+    private double rating;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    private List<CategoryDTO> categories;
+    private List<StreamingDTO> streamings;
+
 }
