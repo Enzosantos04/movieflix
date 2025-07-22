68ffab560d3ff5af489afab11815fa56fb63c74f
enzosdev <enzohfs@gmail.com>
2025-07-22 20:27:36 -0300

    feat: usnado derived queries do JPA para gerar a consulta findMovieByCategories_Id no Postgresql

--- src/main/java/com/movieflix/repository/MovieRepository.java
+++ src/main/java/com/movieflix/repository/MovieRepository.java
@@ -1,7 +1,11 @@
 package com.movieflix.repository;
 
+import com.movieflix.entity.Category;
 import com.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findMovieByCategories_Id(Long categoryId);
 }
