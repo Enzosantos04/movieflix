86c50f3dc5aca97d5f3690b00ee741bc00192b99
enzosdev <enzohfs@gmail.com>
2025-07-21 21:48:30 -0300

    feat: Criar controller para entidade Movie com Endpoints REST

--- src/main/java/com/movieflix/controller/MovieController.java
+++ src/main/java/com/movieflix/controller/MovieController.java
@@ -1,10 +1,33 @@
 package com.movieflix.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.movieflix.dto.MovieDTO;
+import com.movieflix.service.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movieflix/movie")
 public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<MovieDTO> saveMovie(@RequestBody MovieDTO movie){
+        MovieDTO movieDTO = movieService.saveMovie(movie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(movieDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieDTO>> findAll(){
+        List<MovieDTO> movieDTOS = movieService.findAll();
+        return ResponseEntity.ok(movieDTOS);
+    }
 }
