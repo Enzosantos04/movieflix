1d820b11cf1e65ec298ef5ce6f038f42dc807f75
enzosdev <enzohfs@gmail.com>
2025-07-21 21:46:33 -0300

    feat: cria regra de negocio para a entidade movie

--- src/main/java/com/movieflix/service/MovieService.java
+++ src/main/java/com/movieflix/service/MovieService.java
@@ -1,13 +1,45 @@
 package com.movieflix.service;
 
+import com.movieflix.dto.MovieDTO;
+import com.movieflix.entity.Category;
+import com.movieflix.entity.Movie;
+import com.movieflix.mapper.MovieMapper;
+import com.movieflix.repository.CategoryRepository;
 import com.movieflix.repository.MovieRepository;
+import com.movieflix.repository.StreamingRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
     private final MovieRepository movieRepository;
+    private final MovieMapper movieMapper;
+    private final CategoryRepository categoryRepository;
+    private final StreamingRepository streamingRepository;
 
-    public MovieService(MovieRepository movieRepository) {
+    public MovieService(MovieRepository movieRepository, MovieMapper movieMapper, CategoryRepository categoryRepository, StreamingRepository streamingRepository) {
         this.movieRepository = movieRepository;
+        this.movieMapper = movieMapper;
+        this.categoryRepository = categoryRepository;
+        this.streamingRepository = streamingRepository;
     }
+
+    public List<MovieDTO> findAll(){
+        List<Movie> movies = movieRepository.findAll();
+       return movies.stream()
+                .map(movieMapper::map)
+                .collect(Collectors.toList());
+
+    }
+
+    public MovieDTO saveMovie(MovieDTO movieDTO){
+        Movie newMovie = movieMapper.map(movieDTO);
+        newMovie = movieRepository.save(newMovie);
+        return movieMapper.map(newMovie);
+    }
+
+
+
 }
