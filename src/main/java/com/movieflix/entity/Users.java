a631e17c4d6cd9b345112b04777ef89b3d3333dc
enzosdev <enzohfs@gmail.com>
2025-07-26 16:42:57 -0300

    feat: cria entidade Users

---
+++ src/main/java/com/movieflix/entity/Users.java
@@ -0,0 +1,24 @@
+package com.movieflix.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
