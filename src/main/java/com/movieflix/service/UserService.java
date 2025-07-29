7f3dc12d5bd2e4e6e692c7863f644c78ece990f4
enzosdev <enzohfs@gmail.com>
2025-07-29 15:58:08 -0300

    feat: Cria regra de negocio para a entidade users

--- src/main/java/com/movieflix/service/UserService.java
+++ src/main/java/com/movieflix/service/UserService.java
@@ -1,9 +1,23 @@
 package com.movieflix.service;
 
+import com.movieflix.dto.UserDTO;
+import com.movieflix.entity.User;
+import com.movieflix.mapper.UserMapper;
 import com.movieflix.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private final UserRepository usersRepository;
+    private final  UserRepository usersRepository;
+    private final UserMapper userMapper;
+
+public UserService(UserRepository usersRepository, UserMapper userMapper) {
+        this.usersRepository = usersRepository;
+        this.userMapper = userMapper;
+    }
+public UserDTO register(UserDTO userDTO) {
+        User user = userMapper.map(userDTO);
+        User savedUser = usersRepository.save(user);
+        return userMapper.map(savedUser);
+    }
 }
