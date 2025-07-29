ab2caed4b65702e1eed7c31067f599a7db57179e
enzosdev <enzohfs@gmail.com>
2025-07-29 15:55:40 -0300

    feat: cria mapper para traduzir o DTO para entidade

--- src/main/java/com/movieflix/mapper/UserMapper.java
+++ src/main/java/com/movieflix/mapper/UserMapper.java
@@ -1,4 +1,27 @@
 package com.movieflix.mapper;
 
+import com.movieflix.dto.UserDTO;
+import com.movieflix.entity.User;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class UserMapper {
+    public User map(UserDTO dto){
+         User user = new User();
+        user.setId(dto.getId());
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        return user;
+    }
+
+    public UserDTO map(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setEmail(user.getEmail());
+        dto.setPassword(user.getPassword());
+        return dto;
+    }
 }
