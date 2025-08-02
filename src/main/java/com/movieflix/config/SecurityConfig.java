25c51198bcfb3414fed868a55bc2f15fea224ffa
enzosdev <enzohfs@gmail.com>
2025-08-02 17:58:01 -0300

    refactor: adiciona mais dos requestMatchers para dar permissao as rotas do swagger

--- src/main/java/com/movieflix/config/SecurityConfig.java
+++ src/main/java/com/movieflix/config/SecurityConfig.java
@@ -38,6 +38,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/movieflix/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/movieflix/auth/login").permitAll()
                         //para qualquer outra requisição, é necessário estar autenticado
+                        .requestMatchers(HttpMethod.GET, "/api/api-docs/**").permitAll() // permite acesso à documentação da API
+                        .requestMatchers(HttpMethod.GET, "/swagger/**").permitAll() // permite acesso ao Swagger UI
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class) // adiciona o filtro de segurança antes do filtro de autenticação padrão
