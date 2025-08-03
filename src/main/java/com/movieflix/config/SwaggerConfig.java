e0d68c2978fa407bc9f720974a5198ccb655c570
enzosdev <enzohfs@gmail.com>
2025-08-02 23:33:11 -0300

    feat: cria SwaggerConfig pra configurar a documentacao da API retornando um Objeto OpenAPI configurado com as infos da API

--- src/main/java/com/movieflix/config/SwaggerConfig.java
+++ src/main/java/com/movieflix/config/SwaggerConfig.java
@@ -1,4 +1,40 @@
 package com.movieflix.config;
 
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer"
+)
 public class SwaggerConfig {
+    @Bean
+    //metodo para configurar a documentação da API
+    // Retorna um objeto OpenAPI configurado com informações da API
+    public OpenAPI getOpenAPI(){
+
+        // Cria um objeto Contact com informações de contato do desenvolvedor
+        Contact contact = new Contact();
+        contact.name("Enzo");
+        contact.email("enzosdev@gmail.com");
+        // Cria um objeto Info com detalhes da API
+        // Define o título, versão e descrição da API
+       Info info = new Info();
+        info.title("MovieFlix API");
+        info.version("v1");
+        info.description("API documentation for the MovieFlix application");
+        info.contact(contact);
+
+
+        return new OpenAPI()
+                .info(info);
+    }
 }
